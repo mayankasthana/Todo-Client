078ab750dd118bb04fecfb61d97a810cfee98ba0
mayank <mayankasthana1993@gmail.com>
2014-10-20 15:22:08 +0530

    Added input box for new task, added model for new task, new function for adding new task. Also changed the look of list

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,9 +11,16 @@ angular.module('todoApp')
   .controller('TodoCtrl', function ($scope,$http) {
       $http.get('http://localhost:8000/public/api/tasks').success(function(data){
           $scope.tasks = data;
-          console.log($scope.tasks);
       });
       $http.get('http://localhost:8000/public/api/users').success(function(data){
           $scope.users = data;
       });
+      $scope.addNewTask = function(input){
+          console.log(input);
+          //get currently logged in user token and send
+          //Put 
+          $http.put('http://localhost:8000/public/api/addTask').success(function(task){
+              $scope.tasks.push(task);
+          });
+      }
   });
