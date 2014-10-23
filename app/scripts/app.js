9d61ffc33f634060478bedd6ed2f3531397563c6
mayank <mayankasthana1993@gmail.com>
2014-10-24 02:55:31 +0530

    String.contains polyfill

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -13,4 +13,8 @@ angular
             'ngTouch'
         ])
         ;
-
+if ( !String.prototype.contains ) {
+    String.prototype.contains = function() {
+        return String.prototype.indexOf.apply( this, arguments ) !== -1;
+    };
+}
